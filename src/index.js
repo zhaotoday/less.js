bbfccaaad8fce5436df0c64f120572f5e0abaf18
zhaotoday <729234283@qq.com>
2018-04-08 00:00:30 +0830

    配置端口

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ require('./middlewares')(app)
 require('./router')(app)
 
 app.listen(consts.PORT, () => {
-  console.log('server is running at http://localhost:3000')
+  console.log(`server is running at http://localhost:${consts.PORT}`)
 })
