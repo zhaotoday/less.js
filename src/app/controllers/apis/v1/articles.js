9fef0f37cd805e07a07df1093f0c6949b81365b8
zhaotoday <729234283@qq.com>
2018-04-13 21:49:01 +0830

    优化

--- src/app/controllers/apis/v1/articles.js
+++ src/app/controllers/apis/v1/articles.js
@@ -3,7 +3,7 @@ module.exports = app => {
 
   return class extends app.$Controller {
     async index (ctx, next) {
-      ctx.response.body = ctx.send({
+      ctx.send({
         status: 200,
         data: await service.find({offset: 0, limit: 10})
       })
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ const router = require('koa-router')()
 
 module.exports = app => {
   require('./routes/articles')(app, router)
-  require('./routes/apis/articles')(app, router)
-  require('./routes/apis/actions/login')(app, router)
+  require('./routes/apis/v1/articles')(app, router)
+  require('./routes/apis/v1/actions/login')(app, router)
 
   app.use(router.routes()).use(router.allowedMethods())
 }
