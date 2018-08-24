72833c25eaac28e5c3d36ad0084a82a3fe870aff
zhaojintian <729234283@qq.com>
2018-08-24 22:10:23 +0800

    优化

--- src/extends/service.js
+++ src/extends/service.js
@@ -2,18 +2,43 @@ module.exports = app => {
   class Service {
     constructor () {
       this.model = null
+      this.hasOrder = false
     }
 
     /**
      * 查询
      * @returns {Promise}
      */
-    async find ({ id = '', attributes = null, offset = 0, limit = 10, where = null, order = [['id', 'DESC']] } = {}) {
+    async find ({ id = '', attributes = null, offset = 0, limit = 10, where = {}, order = [['id', 'DESC']] } = {}) {
       if (id) {
         return this.model.findById(id)
       } else {
         return this.model.findAll({ attributes, offset, limit, where, order })
       }
+    }
+
+    /**
+     * 查询符合条件的上一条记录
+     * @returns {Promise}
+     */
+    async findPrev ({ id = '', attributes = null, where = {}, order = [['id', 'DESC']] } = {}) {
+      if (id) {
+        where = { ...where, id: { $lt: id } }
+      }
+
+      return (await this.model.findAll({ attributes, limit: 1, where, order }))[0] || null
+    }
+
+    /**
+     * 查询符合条件的下一条记录
+     * @returns {Promise}
+     */
+    async findNext ({ id = '', attributes = null, where = {} } = {}, order = [['id', 'ASC']]) {
+      if (id) {
+        where = { ...where, id: { $gt: id } }
+      }
+
+      return (await this.model.findAll({ attributes, limit: 1, where, order }))[0] || null
     }
 
     /**
@@ -31,6 +56,11 @@ module.exports = app => {
      * @returns {Promise}
      */
     async create ({ body = null } = {}) {
+      if (this.hasOrder) {
+        const findPrevRes = await this.findPrev()
+        body = { ...body, order: findPrevRes ? findPrevRes.id + 1 : 1 }
+      }
+
       return this.model.create(body)
     }
 
@@ -48,7 +78,7 @@ module.exports = app => {
      * 获取记录总数
      * @returns {Promise}
      */
-    async count ({ where = null } = {}) {
+    async count ({ where = {} } = {}) {
       return this.model.count({ where })
     }
   }
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,11 +1,11 @@
 module.exports = app => {
+  require('./static')(app)
   require('./send')(app)
   // 处理错误
   require('./errors')(app)
   // cors 一定要在 jwt 之前，否则 options 请求返回 401
   require('./cors')(app)
   require('./jwt')(app)
-  require('./static')(app)
   require('./body')(app)
   require('./views')(app)
   require('./isMobile')(app)
