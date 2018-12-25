219ef8c0c16c8452ecc434901fad54c79d66dd43
zhaojintian <729234283@qq.com>
2018-12-25 11:21:03 +0800

    启动时初始化

--- src/extends/initialize.js
+++ src/extends/initialize.js
@@ -1,9 +1,13 @@
 const consts = require('../utils/consts')
 
 module.exports = app => {
-  const initModule = require('../utils/load-module')(`${consts.DIRS.EXTENDS}/init.js`)
+  return () => {
+    const initializeModule = require('../utils/load-module')(`${consts.DIRS.EXTENDS}/initialize.js`)
 
-  if (initModule) {
-    initModule(app)
+    if (initializeModule) {
+      initializeModule(app)
+    }
+
+    return app
   }
 }
