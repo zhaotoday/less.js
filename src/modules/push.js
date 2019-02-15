e521facb7e7a0d6e21a0f30591390b176816273d
zhaojintian <729234283@qq.com>
2019-02-15 22:37:42 +0800

    优化

--- src/modules/push.js
+++
@@ -1,9 +0,0 @@
-const JPush = require('jpush-async/lib/JPush/JPushAsync')
-
-module.exports = app => {
-  const { APP_KEY, MASTER_SECRET } = app.$config.PUSH
-
-  JPush.client = JPush.buildClient(APP_KEY, MASTER_SECRET)
-
-  return JPush
-}
--- src/modules/sms.js
+++
@@ -1,6 +0,0 @@
-const QcloudSms = require('qcloudsms_js')
-
-module.exports = app => {
-  const { APP_ID, APP_KEY } = app.$config.SMS
-  return QcloudSms(APP_ID, APP_KEY)
-}
