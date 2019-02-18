a0e475d537dfde181d7dac5e3ded58fd0544d026
zhaojintian <729234283@qq.com>
2019-02-18 15:23:37 +0800

    优化

--- src/modules/time.js
+++ src/modules/time.js
@@ -3,14 +3,16 @@ const format = require('date-fns/format')
 const DATE_FORMATTER = 'YYYY-MM-DD'
 const TIME_FORMATTER = 'YYYY-MM-DD HH:mm'
 
+const _getNow = () => new Date().getTime()
+
 module.exports = app => ({
   getDate (time) {
-    return format(time, DATE_FORMATTER)
+    return format(time || _getNow(), DATE_FORMATTER)
   },
   getTime (time) {
-    return format(time, TIME_FORMATTER)
+    return format(time || _getNow(), TIME_FORMATTER)
   },
   format (time, formatter) {
-    return format(time, formatter)
+    return format(time || _getNow(), formatter)
   }
 })
