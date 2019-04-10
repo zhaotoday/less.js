9431c7fc890edc527ef8985de5a9b96389c14bd3
zhaojintian <729234283@qq.com>
2019-04-10 15:26:21 +0800

    优化

--- src/extends/resources.js
+++ src/extends/resources.js
@@ -1,24 +1,30 @@
 module.exports = (router, path, controller) => {
   const methods = ['get', 'post', 'put', 'del']
+  const getHandler = method => {
+    return async (ctx, next) => {
+      const { id } = ctx.params
+
+      // id 存在且非法
+      if (id && !+id) {
+        next()
+      } else {
+        if (controller.requiresAuth) {
+          const verifyRes = await controller.verify(ctx)
+          await controller[method](ctx, next, verifyRes)
+        } else {
+          await controller[method](ctx, next)
+        }
+        next()
+      }
+    }
+  }
 
   methods.forEach(method => {
-    if (controller[method]) {
-      router[method](`${path}/:id?`, async (ctx, next) => {
-        const { id } = ctx.params
+    const handler = getHandler(method)
 
-        // id 存在且非法
-        if (id && !+id) {
-          next()
-        } else {
-          if (controller.requiresAuth) {
-            const verifyRes = await controller.verify(ctx)
-            await controller[method](ctx, next, verifyRes)
-          } else {
-            await controller[method](ctx, next)
-          }
-          next()
-        }
-      })
+    if (controller[method]) {
+      router[method](`${path}/:id?`, handler(method))
+      controller.del && router.del(`${path}/:ids`, handler(method))
     }
   })
 }
